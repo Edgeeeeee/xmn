40abc63903afc914688a3dc94c6ee0e07b182117
Edgeeeeee <1602278333@qq.com>
2019-04-23 23:25:12 +0800

    Only for learning

---
+++ 包/p01.py
@@ -0,0 +1,17 @@
+# 包含一个学生类
+# 一个sayhello函数
+# 一个打印语句
+
+class Student():
+    def __init__(self,name = "noname",age = 18):
+        self.name = name
+        self.age = age
+
+    def say(self):
+        print("My name is {0}".format(self.name))
+
+def sayhello():
+    print("Hi,guys!")
+
+print("p01是一个模块。。。。。。。。。  最好不要写在模块中写这种函数")
+# 在p02中调用
