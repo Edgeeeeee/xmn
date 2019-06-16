fb5c8b8afaa992ef03ec22d664db99c91ae4f5a8
Edgeeeeee <1602278333@qq,com>
2019-06-16 09:53:56 +0800

    123

---
+++ log/01.py
@@ -0,0 +1,18 @@
+import logging
+
+LOG_FORMAT = "%(asctime)s----%(levelname)s----%(message)s"
+
+# 简单的配置。
+logging.basicConfig(filename="logfile.log", level=logging.DEBUG, format=LOG_FORMAT)
+
+
+logging.debug("This is a debug log")
+logging.info("This is a info log")
+logging.warning("This is a warning log")
+logging.error("This is a error log")
+logging.critical("This is a critical log")
+
+logging.basicConfig(level=logging.WARNING)  # 没有作用。
+# 另外一种写法
+logging.log(logging.DEBUG, "this is a dubug log")
+logging.log(logging.WARNING, "this is a WARINING log")
---
+++ 多线程/note.md
@@ -0,0 +1,4 @@
+# 多线程
+- [多线程](https://www.cnblogs.com/jokerbj/p/7460260.html)
+- [全局解释锁](http://www.dabeaz.com/python/UnderstandingGIL.pdf)
+- [全局解释锁](https://www.cnblogs.com/SuKiWX/p/8804974.html)
